4d4b51f9688ebfeb298805bd15073506f9bef96e
bogdan.stefan <bogdan.stefan@EOSIT-BOGDAN.eosit.local>
2018-11-23 11:47:40 +0200

    pics and awesome

--- src/app/home/home-page.component.ts
+++ src/app/home/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CatalogService } from '../services/catalog.service';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 
 @Component({
   selector: 'app-home-page',
