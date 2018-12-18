17d8238988aa41d63813169a430bbb1ceee003a6
Radu <rpaise@gmail.com>
2018-12-18 07:43:29 +0200

    inventar screen

--- src/app/inventar/inventar-add.component.ts
+++ src/app/inventar/inventar-add.component.ts
@@ -13,13 +13,12 @@ export class InventarAddComponent implements OnInit {
 
   @Output() result: EventEmitter<Boolean> = new EventEmitter<Boolean>();
   @Input() accoutList: any;
-  public code: any = '';
   public name: any = '';
   public pret: any = '';
   public nrArticole: Number = 1;
   public listaArticole: any = [];
   public accountId: any;
-  public valueList: Number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  public valueList: Number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   constructor(private apiService: ApiService,
     public activeModal: NgbActiveModal) {
@@ -35,17 +34,16 @@ export class InventarAddComponent implements OnInit {
     this.name = '';
     this.pret = '';
     this.listaArticole = [];
+    this.changeNrArticole();
   }
 
   valid(): Boolean {
     for (let x = 0; x < this.listaArticole.length; x++) {
       if (this.listaArticole[x].code === '') { return false; }
-      if (this.listaArticole[x].code === this.code) { return false; }
       for (let y = x; y < this.listaArticole.length; y++) {
         if ((this.listaArticole[x].code === this.listaArticole[y].code) && (x !== y)) { return false; }
       }
     }
-    if (this.code === '') { return false; }
     if (this.name === '') { return false; }
     if (!Utils.stringCanContainOnlyNumbers(this.pret) || (Number(this.pret) === 0)) { return false; }
     if (this.accoutList.length === 0) { return false; }
@@ -53,7 +51,7 @@ export class InventarAddComponent implements OnInit {
   }
 
   doSave() {
-      const outlist = [this.code];
+      const outlist = [];
       for (const elem of this.listaArticole) { outlist.push(elem.code); }
       const output = {
         pret: this.pret,
@@ -72,7 +70,7 @@ export class InventarAddComponent implements OnInit {
 
   changeNrArticole() {
     this.listaArticole = [];
-    for (let x = 1; x < this.nrArticole; x++ ) { this.listaArticole.push({code: ''}); }
+    for (let x = 0; x < this.nrArticole; x++ ) { this.listaArticole.push({code: ''}); }
   }
 
   cancel() {
